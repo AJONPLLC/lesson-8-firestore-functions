d0fbdd54327f00834494be0d108d7540f61a2b01
Alex Patterson <admin@ajonp.com>
2018-12-22 15:12:39 -0500

    Initial test setup

--- functions/src/gitcommit/index.ts
+++ functions/src/gitcommit/index.ts
@@ -1,6 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as nodegit from 'nodegit';
-import { rmdirSync } from 'fs';
+import * as fse from 'fs-extra';
+import * as path from 'path';
 import * as os from 'os';
 
 export const gitReadCommit = functions.firestore
@@ -8,17 +9,25 @@ export const gitReadCommit = functions.firestore
   .onCreate(async snapshot => {
     const book = snapshot.data();
     const token = functions.config().git.token; //Trying to keep this secret, run firebase functions:config:set git.token=<your_token>
-    const repoOwner = "AJONPLLC";
-    const repoName = "lesson-8-hugo";
-    const repoUrl = "https://github.com/" + repoOwner + "/" + repoName + ".git";
+    const repoOwner = 'AJONPLLC';
+    const repoName = 'lesson-8-hugo';
+    const repoUrl = 'https://github.com/' + repoOwner + '/' + repoName + '.git';
+    let debug = 0;
     const opts = {
       fetchOpts: {
         callbacks: {
-          credentials: function() {
-            return nodegit.Cred.userpassPlaintextNew(token, "x-oauth-basic");
-          },
-          certificateCheck: function() {
+          certificateCheck: () => {
+            // github will fail cert check on some OSX machines
+            // this overrides that check
             return 1;
+          },
+          credentials: (url, userName) => {
+             // avoid infinite loop when authentication agent is not loaded
+             if (debug++ > 5){
+              console.log('Failed too often, bailing.')
+              throw new Error("Authentication agent not loaded.");
+            }
+            return nodegit.Cred.sshKeyFromAgent(userName);
           }
         }
       }
@@ -26,31 +35,66 @@ export const gitReadCommit = functions.firestore
 
     console.log(book);
 
-    nodegit.Clone(repoUrl, os.tmpdir(),opts)
-    .then((repo) => {
-      return repo.getCommit("676bdb8071cd435499603b8f28f4cc5ee72c4f72");
-    })
-    .then((commit)=>{
-      return commit.getEntry("README.md");
-    })
-      // Get the blob contents from the file.
-  .then(function(entry) {
-    // Patch the blob to contain a reference to the entry.
-    return entry.getBlob().then(function(blob) {
-      blob.entry = entry;
-      return blob;
-    });
-  })
-  // Display information about the blob.
-  .then(function(blob) {
-    // Show the path, sha, and filesize in bytes.
-    console.log(blob.entry.path() + blob.entry.sha() + blob.rawsize() + "b");
+    try {
+      fse.emptyDirSync(os.tmpdir());
+      console.log('Cloning Repo');
+      const repo = await nodegit.Clone.clone(repoUrl, os.tmpdir(), opts);
+      fse.ensureDirSync(path.join(repo.workdir(), `content/books/`));
+      const filePath = path.join(
+        repo.workdir(),
+        `content/books/`,
+        `${book.title}.md`
+      );
+      console.log('Writing File', filePath);
+      fse.writeFileSync(filePath,
+`+++
+title = "${book.description}"
+date = ${new Date().toISOString()}
+images = ["https://res.cloudinary.com/ajonp/image/upload/v1545282630/ajonp-ajonp-com/8-lesson-firestore-functions/bookExample.png"]
++++
 
-    // Show a spacer.
-    console.log(Array(72).join("=") + "\n\n");
-
-    // Show the entire file.
-    console.log(String(blob));
-  })
-  .catch(function(err) { console.log(err); });
+${book.description}
+`
+);
+      const index = await repo.refreshIndex();
+      console.log('Adding to Index:', filePath);
+      await index.addByPath(`content/books/${book.title}.md`);
+      await index.write();
+      const oid = await index.writeTree();
+      const head = await nodegit.Reference.nameToId(repo, 'HEAD');
+      const parent = await repo.getCommit(head);
+      const author = nodegit.Signature.now('ajonp', 'developer@ajonp.com');
+      const committer = nodegit.Signature.now('ajonp', 'developer@ajonp.com');
+      const commitId = await repo.createCommit(
+        'HEAD',
+        author,
+        committer,
+        'message',
+        oid,
+        [parent]
+      );
+      console.log('New Commit: ', commitId);
+      console.log('Attempting Push');
+      const remote = await repo.getRemote('origin');
+      return remote.push(['refs/heads/master:refs/heads/master'], {
+        callbacks: {
+          certificateCheck: () => {
+            // github will fail cert check on some OSX machines
+            // this overrides that check
+            return 1;
+          },
+          credentials: (url, userName) => {
+             // avoid infinite loop when authentication agent is not loaded
+             if (debug++ > 5){
+              console.log('Failed too often, bailing.')
+              throw new Error("Authentication agent not loaded.");
+            }
+            return nodegit.Cred.sshKeyFromAgent(userName);
+          }
+        }
+      });
+    } catch (err) {
+      console.log(err);
+      return false;
+    }
   });
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,14 +5,14 @@ import * as notifications from './notifications';
 import * as likes from './likes';
 import * as gitcommit from './gitcommit';
 
-//Notifications
-export const sendOnBookCreate = notifications.sendOnBookCreate;
-export const subscribeToTopic = notifications.subscribeToTopic;
-export const unsubscribeFromTopic = notifications.unsubscribeFromTopic;
+// //Notifications
+// export const sendOnBookCreate = notifications.sendOnBookCreate;
+// export const subscribeToTopic = notifications.subscribeToTopic;
+// export const unsubscribeFromTopic = notifications.unsubscribeFromTopic;
 
-//Books
-export const bookLikesChange = likes.bookLikesChange;
-export const bookLikesCountDelete = likes.bookLikesCountDelete;
+// //Books
+// export const bookLikesChange = likes.bookLikesChange;
+// export const bookLikesCountDelete = likes.bookLikesCountDelete;
 
 //Git
 export const gitReadCommit = gitcommit.gitReadCommit;
